1711e77abf385704883bc1174f4ed470d73dcd76
Jonathan POULLAIN <Jonathan@MacBook-Pro-de-Jonathan.local>
2016-03-15 15:15:14 +0100

    ajout langue fr

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@posteur.go', 'name' => 'post'],
 
     /*
     |--------------------------------------------------------------------------
