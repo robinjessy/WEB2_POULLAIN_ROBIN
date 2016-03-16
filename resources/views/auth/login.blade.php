6727a35e351dc6b3da617410f4acc9761340ca03
Hyce <jon.poullain@gmail.com>
2016-03-16 17:46:12 +0100

    Refresh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
+                            <label class="col-md-4 control-label">Passwords</label>
 
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
