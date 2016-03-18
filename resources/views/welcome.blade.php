b6aaaddec3ec55f4b6d6f2dbff29cf89bbcba526
Jessy <robinjessyiim@gmail.com>
2016-03-18 12:32:42 +0100

    ajoute de la page contact + acceuil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+    <div class="jumbotron">
+        <h1>Plate-forme BAP</h1>
+        <p>Par binôme, vous devrez réaliser une plate-forme pour l’IIM sur laquelle il sera possible de soumettre des projets BAP.</p>
 
-                    <div class="panel-body">
-                        hellooo
-                    </div>
-                </div>
-            </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+            <h2>Projet</h2>
+            <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
+        </div>
+        <div class="col-sm-4">
+            <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+            <h2>Articles</h2>
+            <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+        </div>
+        <div class="col-sm-4">
+            <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+            <h2>Contact</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
         </div>
     </div>
+   <br>
+
 @endsection
